5f79a7fbba6fa7b620037eec892d401eff6193c6
Joe <jconstan@bu.edu>
2017-09-15 20:45:14 -0700

    optimizing

--- app/src/main/java/com/example/joe/smashhandbook/DBHelper.java
+++ app/src/main/java/com/example/joe/smashhandbook/DBHelper.java
@@ -189,7 +189,7 @@ public class DBHelper extends SQLiteOpenHelper{
         desc[3] = "Captain Falcon is one of the most exciting characters to watch- he has flashy combos and often relies on airdodge reads. However, his recovery is very linear and easy to interrupt.";
         desc[4] = "Charizard is heavy and slow. With those traits, though, come a surplus of kill power, and two double jumps.";
         desc[5] = "Cloud is clearly the best character in doubles, and top 3 in singles. He has amazing aerials, amazing movement options, and great mixups. His only staggering weakness is his short and linear recovery.";
-        desc[6]= "Corrin is a great juggler and good at racking up damage. Her kill power is nothing to laught at either, and she is more mobile than she seems.";
+        desc[6] = "Corrin is a great juggler and good at racking up damage. Her kill power is nothing to laught at either, and she is more mobile than she seems.";
         desc[7] = "Dark Pit is the least used character in the game. There are no glaring weaknesses with Dark Pit, but there are also no glowing strengths. Dark Pit is a very honest, all-around, average character.";
         desc[8] = "Diddy is a great all around character who can be played aggressively or defensively. His meta has been pushed farther than many characters, though there are still many useful techniques not being used by diddy mains. His play centers around his banana, giving him great stage control and easy kill confirms. Watch ZeRo play to learn the basic bread and butter combos, and watch zinoto to see some unique combos and set ups.";
         desc[9] = "Donkey Kong has been on the rise lately, with HIKARU and Konga doing well, as well as Tweek and Larry using him as a secondary. He is a grappler through and through, and can kill most characters at extremely low percents. He does struggle at landing, though.";
@@ -207,7 +207,7 @@ public class DBHelper extends SQLiteOpenHelper{
         desc[20] = "Link ";
         desc[21] = "Little Mac is definitely one of the most fun characters to play. His ground game is one of the best, and his frame data is ridiculous. He struggles, though, in the air and when recovering. For these reasons he currently has more than a few losing matchups.";
         desc[22] = "Lucario is a scary character to play against, as he has aura on his side. This means that some of his moves can start KOing at extremely early percents when he has some amount of aura.";
-        desc[23] = "Lucas";
+        desc[23] = "Lucas is very similar to ness, with a few key differences. His side special, when used in the air, will go in a straight line, while Ness' goes at a 45 degree angle. He also has a zair, which can be used as a tether or as an attack. etc";
         desc[24] = "Lucina is very similar to marth, with a few exceptions. Lucina has a slightly shorter reach, not because of the length of the sword, but because of the reach of her arm. Her sword has no tipper effect, and the entire length of it does the same amount of damage. So this makes spacing easier,but results in her not having moves as strong as marth's tippers.";
         desc[25] = "Luigi is represented at the highest level by only a single player. He has very low friction, and therefore relies heavily on power shielding for counterattacks. He has endless combos out of down throw";
         desc[26] = "Mario has some of the most exciting combos in the game. Most of them start with a down throw, though certain characters are able to break out of the typical combo chains. Therefore mario mains must mix up their combo strategies. He has decent kill power, but not a lot of true kill combos at any percent.";
@@ -215,9 +215,9 @@ public class DBHelper extends SQLiteOpenHelper{
         desc[28] = "Mega Man is a very zone-based, stage-control character. His numerous projectiles make him frustrating to play against, and";
         desc[29] = "Meta Knight";
         desc[30] = "Mewtwo is a bit of a glass cannon. He has some exciting combos and he is very fun to watch. He is floaty, so can be difficult to combo, but he is also very light and will usually die very early.";
-        desc[31] = "Mii Swordfighter desc";
-        desc[32] = "Mii Gunner desc";
-        desc[33] = "Mii Brawler desc";
+        desc[31] = "Mii Swordfighter";
+        desc[32] = "Mii Gunner";
+        desc[33] = "Mii Brawler";
         desc[34] = "Mr. G&W is 2D, and is therefore one of the lightest characters in the game. ..";
         desc[35] = "Ness has the strongest throw in the game in his back throw. He can get gimped by characters that can absorb his up special, like Rosalina or Ness/Lucas.";
         desc[36] = "Olimar is on the rise, with players like Shuton pushing him farther and farther. He requires micrmanagement and knowledge of the pikmin to play well.";
