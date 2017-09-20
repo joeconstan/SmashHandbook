35a904602d5df00faf90ee1ff7802faceb9d7075
Joe <jconstan@bu.edu>
2017-09-19 22:50:12 -0700

    pre alpha push

--- app/src/main/java/com/example/joe/smashhandbook/Characters.java
+++ app/src/main/java/com/example/joe/smashhandbook/Characters.java
@@ -3,6 +3,7 @@ package com.example.joe.smashhandbook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
